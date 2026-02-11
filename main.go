17ca485a260452629bf3a3ab49081396ba4846da
whitekotan0 <gcat5770@gmail.com>
2026-02-11 12:41:56 +0200

    feat: handling invalid method

--- main.go
+++ main.go
@@ -31,6 +31,11 @@ func main() {
 }
 
 func handlePage(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "this is GET methd!", http.StatusMethodNotAllowed)
+		return
+	}
 	http.ServeFile(w, r, "index.html")
 
 }
