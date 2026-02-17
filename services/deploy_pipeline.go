4ddb571593cba918ebd528e630a8bb49a556c0e3
whitekotan <gcat5770@gmail.com>
2026-02-17 19:17:26 +0200

    feat: add facade func for build project

--- services/deploy_pipeline.go
+++ services/deploy_pipeline.go
@@ -1,10 +1,23 @@
 package services
 
 import (
+	"cdcat/types"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+func BuildProjectPipeline(request types.Request) types.UserProject {
+	userProject := CreateUserProject(request)
+
+	cloneErr := CloneUserProject(userProject)
+	if cloneErr != nil {
+		fmt.Println("can't clone repo")
+	}
+	BuildUserProject(userProject)
+
+	return userProject
+}
 
 func DeployPipeline(filePath string, bucketName string, fileName string, cloudflareConfig *s3.Client) {
 
