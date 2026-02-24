0ccd03f698f533b660622958831e2f3ddc7f15f9
startupman <ownerofmylife2@gmail.com>
2026-02-25 01:05:37 +0200

    feat: add schema for user project

--- storage/shema.sql
+++ storage/shema.sql
@@ -4,10 +4,14 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT UNIQUE NOT NULL,
     plan TEXT NOT NULL DEFAULT 'free'
 
-
-)
+);
 
 
 CREATE TABLE IF NOT EXISTS projects (
-    
+
+    id  SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    github_link TEXT NOT NULL,
+    web_link  TEXT 
 )
