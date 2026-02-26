6f1780112300a34c1a6f674cb944e3c42fc7aabc
whitekotan <gcat5770@gmail.com>
2026-02-26 09:50:13 +0200

    feat: change db connector

--- storage/connectDB.go
+++ storage/connectDB.go
@@ -1,21 +1,29 @@
-package db
+package storage
 
 import (
 	"context"
 	"fmt"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func initDB() {
+func InitDB() {
 
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		fmt.Println("can't connecd db")
-		os.Exit(1)
+		fmt.Println("Error connecting", err)
+		return
+	}
+	defer pool.Close()
+
+	var name string
+	err = pool.QueryRow(context.Background(), "SELECT name FROM users WHERE id=$1", 1).Scan(&name)
+	if err != nil {
+		fmt.Println("query error", err)
+		return
 	}
 
-	defer conn.Close(context.Background())
+	fmt.Println("User name", name)
+
 }
